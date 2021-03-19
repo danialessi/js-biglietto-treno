d5e8ac6a3ce45520b10ad32d9c951b80a0b5f6ad
Daniele Alessi <daniele.alessi@hotmail.it>
2021-03-19 17:11:01 +0100

    aggiunte condizioni

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero
+// chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero, sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio
 
 // Chilometri 
 var chilometri = prompt('Quanti chilometri vuoi percorrere?');
@@ -12,13 +12,20 @@ console.log(numeroEtà);
 
 // Prezzo biglietto
 var prezzo = 0.21 * numeroChilometri;
-console.log(prezzo);
 
 // Sconto 20%
-// var scontoMinorenni;
 var percent20 = (prezzo / 100) * 20;
+var prezzoMinorenni = prezzo - percent20;
 
+// Sconto 40% 
+var percent40 = (prezzo / 100) * 40;
+var prezzoOver = prezzo - percent40;
 
-// if (numeroEtà < 18) {
-//     scontoMinorenni = (prezzo - (percent
-// }
+// prezzo viaggio 
+if (numeroEtà < 18) {
+    alert(prezzoMinorenni);
+} else if (numeroEtà > 65) {
+    alert(prezzoOver);
+} else {
+    alert(prezzo)
+}
